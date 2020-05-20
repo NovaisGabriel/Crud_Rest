0a9291756dd61940f07496dd2353fbaa3247e2c5
NovaisGabriel <gabrieel.novais@gmail.com>
2020-05-20 18:52:55 -0300

    Service layer

---
+++ src/main/java/com/crud/rest/services/UserService.java
@@ -0,0 +1,26 @@
+package com.crud.rest.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.crud.rest.entities.User;
+import com.crud.rest.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
